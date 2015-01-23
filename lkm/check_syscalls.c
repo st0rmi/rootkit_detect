269e9c02203c678cbef9aa24703044f8cb9cd3f9
Martin Herrmann <martin.herrmann@tum.de>
2015-01-23 22:38:08 +0100

    mher: asdfasdf

--- lkm/check_syscalls.c
+++ lkm/check_syscalls.c
@@ -196,22 +196,22 @@ check_syscalls (void)
 	
 	memcpy(tmp, (void *) sysmap_packet_rcv, 64);
 	memset(message, 0, 128);
-	sprintf(message, "packet_rcv      - %8X %8X %8X %8X\n", (unsigned int) *tmp, (unsigned int) *(tmp+4), (unsigned int) *(tmp+8),
-		(unsigned int) *(tmp+12));
+	sprintf(message, "packet_rcv      - %08X %08X %08X %08X\n", (unsigned int) *tmp, (unsigned int) *(tmp+1), (unsigned int) *(tmp+2),
+		(unsigned int) *(tmp+3));
 	memset(message+127, '\0', 1);
 	write_to_file(fd, message, strlen(message));
 	
 	memcpy(tmp, (void *) sysmap_packet_rcv_spkt, 64);
 	memset(message, 0, 128);
-	sprintf(message, "packet_rcv_spkt - %8X %8X %8X %8X\n", (unsigned int) *tmp, (unsigned int) *(tmp+4), (unsigned int) *(tmp+8),
-		(unsigned int) *(tmp+12));
+	sprintf(message, "packet_rcv_spkt - %08X %08X %08X %08X\n", (unsigned int) *tmp, (unsigned int) *(tmp+1), (unsigned int) *(tmp+2),
+		(unsigned int) *(tmp+3));
 	memset(message+127, '\0', 1);
 	write_to_file(fd, message, strlen(message));
 	
 	memcpy(tmp, (void *) sysmap_tpacket_rcv, 64);
 	memset(message, 0, 128);
-	sprintf(message, "tpacket_rcv     - %8X %8X %8X %8X\n", (unsigned int) *tmp, (unsigned int) *(tmp+4), (unsigned int) *(tmp+8),
-		(unsigned int) *(tmp+12));
+	sprintf(message, "tpacket_rcv     - %08X %08X %08X %08X\n", (unsigned int) *tmp, (unsigned int) *(tmp+1), (unsigned int) *(tmp+2),
+		(unsigned int) *(tmp+3));
 	memset(message+127, '\0', 1);
 	write_to_file(fd, message, strlen(message));
 	
