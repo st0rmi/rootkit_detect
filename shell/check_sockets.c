47df96d3c23f94362544bcf55dea832b62311ca4
Martin Herrmann <martin.herrmann@tum.de>
2015-01-25 15:49:20 +0100

    mher: C Script 2

--- shell/check_sockets.c
+++ shell/check_sockets.c
@@ -1,7 +1,8 @@
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 
