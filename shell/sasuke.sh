c9a90a97c5c91974b959ab6cde1ec100d168b3df
Martin Herrmann <martin.herrmann@tum.de>
2015-01-25 15:09:06 +0100

    mher: Added cmdline to the output.

--- shell/sasuke.sh
+++ shell/sasuke.sh
@@ -11,15 +11,18 @@ fi
 
 function get_procs
 {
+	echo "Listing potentially maliciously hidden processes."
+	echo "Note that most of these are nothing to worry about."
 	PROC=$(ls /proc | grep -o '[0-9]*')
 	for pid in `seq 0 $PID_MAX`;
 	do
 		kill -0 $pid 1> /dev/null 2> /dev/null 
 		if [ $? -eq 0 ]; then
 			contains=`ls /proc | grep -o $pid | wc -l`
+			cmdline=`cat /proc/$pid/cmdline`
 
 			if [ $contains != "1" ]; then
-				echo "$pid not found"
+				echo "$pid $cmdline"
 			fi
 		fi
 	done;
